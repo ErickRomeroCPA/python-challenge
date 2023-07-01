4f994ba3c44ef1f6168e9b52b0db7132362f76b5
ErickRomeroCPA <erickromerofm@gmail.com>
2023-06-30 18:31:40 -0700

    add labels

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,7 @@ election_data = os.path.join('resources','election_data.csv')
 #counter for total votes
 total_votes = 0
 
+#creating arrays for each candidate's total votes
 charles_votes = []
 diana_votes = []
 raymon_votes = []
@@ -17,10 +18,14 @@ with open(election_data,'r') as csvfile:
     #split data on commas
     csvreader = csv.reader(csvfile,delimiter=',')
 
+    #skip header
     header = next(csvreader)
 
     for row in csvreader:
+        #counting total votes
         total_votes = total_votes + 1
+        
+        #counting votes for each candidate
         candidate = row[2]
         if candidate == 'Charles Casper Stockham':
             charles_votes.append(candidate)
@@ -29,10 +34,12 @@ with open(election_data,'r') as csvfile:
         elif candidate == 'Raymon Anthony Doane':
             raymon_votes.append(candidate)
     
+    #calculating the %'s
     charles_percentage = round((len(charles_votes) / total_votes),5) * 100
     diana_percentage = round((len(diana_votes) / total_votes),5) * 100
     raymon_percentage = round((len(raymon_votes) / total_votes),5) * 100
 
+    #determining the winner of the popular vote
     popular_vote = max(charles_percentage,diana_percentage,raymon_percentage)
 
     if popular_vote == charles_percentage:
